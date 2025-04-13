2e765441905bc3f58001702b455fbadffa58f8fb
primike <avecillasprince@yahoo.com>
2025-04-12 22:33:21 -0400

    Adding randomizer for pokemon

---
+++ randomizer.py
@@ -0,0 +1,620 @@
+pokemon_species_list = [
+    "SPECIES_BULBASAUR",
+    "SPECIES_IVYSAUR",
+    "SPECIES_VENUSAUR",
+    "SPECIES_CHARMANDER",
+    "SPECIES_CHARMELEON",
+    "SPECIES_CHARIZARD",
+    "SPECIES_SQUIRTLE",
+    "SPECIES_WARTORTLE",
+    "SPECIES_BLASTOISE",
+    "SPECIES_CATERPIE",
+    "SPECIES_METAPOD",
+    "SPECIES_BUTTERFREE",
+    "SPECIES_WEEDLE",
+    "SPECIES_KAKUNA",
+    "SPECIES_BEEDRILL",
+    "SPECIES_PIDGEY",
+    "SPECIES_PIDGEOTTO",
+    "SPECIES_PIDGEOT",
+    "SPECIES_RATTATA",
+    "SPECIES_RATICATE",
+    "SPECIES_SPEAROW",
+    "SPECIES_FEAROW",
+    "SPECIES_EKANS",
+    "SPECIES_ARBOK",
+    "SPECIES_PIKACHU",
+    "SPECIES_RAICHU",
+    "SPECIES_SANDSHREW",
+    "SPECIES_SANDSLASH",
+    "SPECIES_NIDORAN_F",
+    "SPECIES_NIDORINA",
+    "SPECIES_NIDOQUEEN",
+    "SPECIES_NIDORAN_M",
+    "SPECIES_NIDORINO",
+    "SPECIES_NIDOKING",
+    "SPECIES_CLEFAIRY",
+    "SPECIES_CLEFABLE",
+    "SPECIES_VULPIX",
+    "SPECIES_NINETALES",
+    "SPECIES_JIGGLYPUFF",
+    "SPECIES_WIGGLYTUFF",
+    "SPECIES_ZUBAT",
+    "SPECIES_GOLBAT",
+    "SPECIES_ODDISH",
+    "SPECIES_GLOOM",
+    "SPECIES_VILEPLUME",
+    "SPECIES_PARAS",
+    "SPECIES_PARASECT",
+    "SPECIES_VENONAT",
+    "SPECIES_VENOMOTH",
+    "SPECIES_DIGLETT",
+    "SPECIES_DUGTRIO",
+    "SPECIES_MEOWTH",
+    "SPECIES_PERSIAN",
+    "SPECIES_PSYDUCK",
+    "SPECIES_GOLDUCK",
+    "SPECIES_MANKEY",
+    "SPECIES_PRIMEAPE",
+    "SPECIES_GROWLITHE",
+    "SPECIES_ARCANINE",
+    "SPECIES_POLIWAG",
+    "SPECIES_POLIWHIRL",
+    "SPECIES_POLIWRATH",
+    "SPECIES_ABRA",
+    "SPECIES_KADABRA",
+    "SPECIES_ALAKAZAM",
+    "SPECIES_MACHOP",
+    "SPECIES_MACHOKE",
+    "SPECIES_MACHAMP",
+    "SPECIES_BELLSPROUT",
+    "SPECIES_WEEPINBELL",
+    "SPECIES_VICTREEBEL",
+    "SPECIES_TENTACOOL",
+    "SPECIES_TENTACRUEL",
+    "SPECIES_GEODUDE",
+    "SPECIES_GRAVELER",
+    "SPECIES_GOLEM",
+    "SPECIES_PONYTA",
+    "SPECIES_RAPIDASH",
+    "SPECIES_SLOWPOKE",
+    "SPECIES_SLOWBRO",
+    "SPECIES_MAGNEMITE",
+    "SPECIES_MAGNETON",
+    "SPECIES_FARFETCHD",
+    "SPECIES_DODUO",
+    "SPECIES_DODRIO",
+    "SPECIES_SEEL",
+    "SPECIES_DEWGONG",
+    "SPECIES_GRIMER",
+    "SPECIES_MUK",
+    "SPECIES_SHELLDER",
+    "SPECIES_CLOYSTER",
+    "SPECIES_GASTLY",
+    "SPECIES_HAUNTER",
+    "SPECIES_GENGAR",
+    "SPECIES_ONIX",
+    "SPECIES_DROWZEE",
+    "SPECIES_HYPNO",
+    "SPECIES_KRABBY",
+    "SPECIES_KINGLER",
+    "SPECIES_VOLTORB",
+    "SPECIES_ELECTRODE",
+    "SPECIES_EXEGGCUTE",
+    "SPECIES_EXEGGUTOR",
+    "SPECIES_CUBONE",
+    "SPECIES_MAROWAK",
+    "SPECIES_HITMONLEE",
+    "SPECIES_HITMONCHAN",
+    "SPECIES_LICKITUNG",
+    "SPECIES_KOFFING",
+    "SPECIES_WEEZING",
+    "SPECIES_RHYHORN",
+    "SPECIES_RHYDON",
+    "SPECIES_CHANSEY",
+    "SPECIES_TANGELA",
+    "SPECIES_KANGASKHAN",
+    "SPECIES_HORSEA",
+    "SPECIES_SEADRA",
+    "SPECIES_GOLDEEN",
+    "SPECIES_SEAKING",
+    "SPECIES_STARYU",
+    "SPECIES_STARMIE",
+    "SPECIES_MR_MIME",
+    "SPECIES_SCYTHER",
+    "SPECIES_JYNX",
+    "SPECIES_ELECTABUZZ",
+    "SPECIES_MAGMAR",
+    "SPECIES_PINSIR",
+    "SPECIES_TAUROS",
+    "SPECIES_MAGIKARP",
+    "SPECIES_GYARADOS",
+    "SPECIES_LAPRAS",
+    "SPECIES_DITTO",
+    "SPECIES_EEVEE",
+    "SPECIES_VAPOREON",
+    "SPECIES_JOLTEON",
+    "SPECIES_FLAREON",
+    "SPECIES_PORYGON",
+    "SPECIES_OMANYTE",
+    "SPECIES_OMASTAR",
+    "SPECIES_KABUTO",
+    "SPECIES_KABUTOPS",
+    "SPECIES_AERODACTYL",
+    "SPECIES_SNORLAX",
+    "SPECIES_ARTICUNO",
+    "SPECIES_ZAPDOS",
+    "SPECIES_MOLTRES",
+    "SPECIES_DRATINI",
+    "SPECIES_DRAGONAIR",
+    "SPECIES_DRAGONITE",
+    "SPECIES_MEWTWO",
+    "SPECIES_MEW",
+    "SPECIES_CHIKORITA",
+    "SPECIES_BAYLEEF",
+    "SPECIES_MEGANIUM",
+    "SPECIES_CYNDAQUIL",
+    "SPECIES_QUILAVA",
+    "SPECIES_TYPHLOSION",
+    "SPECIES_TOTODILE",
+    "SPECIES_CROCONAW",
+    "SPECIES_FERALIGATR",
+    "SPECIES_SENTRET",
+    "SPECIES_FURRET",
+    "SPECIES_HOOTHOOT",
+    "SPECIES_NOCTOWL",
+    "SPECIES_LEDYBA",
+    "SPECIES_LEDIAN",
+    "SPECIES_SPINARAK",
+    "SPECIES_ARIADOS",
+    "SPECIES_CROBAT",
+    "SPECIES_CHINCHOU",
+    "SPECIES_LANTURN",
+    "SPECIES_PICHU",
+    "SPECIES_CLEFFA",
+    "SPECIES_IGGLYBUFF",
+    "SPECIES_TOGEPI",
+    "SPECIES_TOGETIC",
+    "SPECIES_NATU",
+    "SPECIES_XATU",
+    "SPECIES_MAREEP",
+    "SPECIES_FLAAFFY",
+    "SPECIES_AMPHAROS",
+    "SPECIES_BELLOSSOM",
+    "SPECIES_MARILL",
+    "SPECIES_AZUMARILL",
+    "SPECIES_SUDOWOODO",
+    "SPECIES_POLITOED",
+    "SPECIES_HOPPIP",
+    "SPECIES_SKIPLOOM",
+    "SPECIES_JUMPLUFF",
+    "SPECIES_AIPOM",
+    "SPECIES_SUNKERN",
+    "SPECIES_SUNFLORA",
+    "SPECIES_YANMA",
+    "SPECIES_WOOPER",
+    "SPECIES_QUAGSIRE",
+    "SPECIES_ESPEON",
+    "SPECIES_UMBREON",
+    "SPECIES_MURKROW",
+    "SPECIES_SLOWKING",
+    "SPECIES_MISDREAVUS",
+    "SPECIES_UNOWN",
+    "SPECIES_WOBBUFFET",
+    "SPECIES_GIRAFARIG",
+    "SPECIES_PINECO",
+    "SPECIES_FORRETRESS",
+    "SPECIES_DUNSPARCE",
+    "SPECIES_GLIGAR",
+    "SPECIES_STEELIX",
+    "SPECIES_SNUBBULL",
+    "SPECIES_GRANBULL",
+    "SPECIES_QWILFISH",
+    "SPECIES_SCIZOR",
+    "SPECIES_SHUCKLE",
+    "SPECIES_HERACROSS",
+    "SPECIES_SNEASEL",
+    "SPECIES_TEDDIURSA",
+    "SPECIES_URSARING",
+    "SPECIES_SLUGMA",
+    "SPECIES_MAGCARGO",
+    "SPECIES_SWINUB",
+    "SPECIES_PILOSWINE",
+    "SPECIES_CORSOLA",
+    "SPECIES_REMORAID",
+    "SPECIES_OCTILLERY",
+    "SPECIES_DELIBIRD",
+    "SPECIES_MANTINE",
+    "SPECIES_SKARMORY",
+    "SPECIES_HOUNDOUR",
+    "SPECIES_HOUNDOOM",
+    "SPECIES_KINGDRA",
+    "SPECIES_PHANPY",
+    "SPECIES_DONPHAN",
+    "SPECIES_PORYGON2",
+    "SPECIES_STANTLER",
+    "SPECIES_SMEARGLE",
+    "SPECIES_TYROGUE",
+    "SPECIES_HITMONTOP",
+    "SPECIES_SMOOCHUM",
+    "SPECIES_ELEKID",
+    "SPECIES_MAGBY",
+    "SPECIES_MILTANK",
+    "SPECIES_BLISSEY",
+    "SPECIES_RAIKOU",
+    "SPECIES_ENTEI",
+    "SPECIES_SUICUNE",
+    "SPECIES_LARVITAR",
+    "SPECIES_PUPITAR",
+    "SPECIES_TYRANITAR",
+    "SPECIES_LUGIA",
+    "SPECIES_HO_OH",
+    "SPECIES_CELEBI",
+    "SPECIES_TREECKO",
+    "SPECIES_GROVYLE",
+    "SPECIES_SCEPTILE",
+    "SPECIES_TORCHIC",
+    "SPECIES_COMBUSKEN",
+    "SPECIES_BLAZIKEN",
+    "SPECIES_MUDKIP",
+    "SPECIES_MARSHTOMP",
+    "SPECIES_SWAMPERT",
+    "SPECIES_POOCHYENA",
+    "SPECIES_MIGHTYENA",
+    "SPECIES_ZIGZAGOON",
+    "SPECIES_LINOONE",
+    "SPECIES_WURMPLE",
+    "SPECIES_SILCOON",
+    "SPECIES_BEAUTIFLY",
+    "SPECIES_CASCOON",
+    "SPECIES_DUSTOX",
+    "SPECIES_LOTAD",
+    "SPECIES_LOMBRE",
+    "SPECIES_LUDICOLO",
+    "SPECIES_SEEDOT",
+    "SPECIES_NUZLEAF",
+    "SPECIES_SHIFTRY",
+    "SPECIES_TAILLOW",
+    "SPECIES_SWELLOW",
+    "SPECIES_WINGULL",
+    "SPECIES_PELIPPER",
+    "SPECIES_RALTS",
+    "SPECIES_KIRLIA",
+    "SPECIES_GARDEVOIR",
+    "SPECIES_SURSKIT",
+    "SPECIES_MASQUERAIN",
+    "SPECIES_SHROOMISH",
+    "SPECIES_BRELOOM",
+    "SPECIES_SLAKOTH",
+    "SPECIES_VIGOROTH",
+    "SPECIES_SLAKING",
+    "SPECIES_NINCADA",
+    "SPECIES_NINJASK",
+    "SPECIES_SHEDINJA",
+    "SPECIES_WHISMUR",
+    "SPECIES_LOUDRED",
+    "SPECIES_EXPLOUD",
+    "SPECIES_MAKUHITA",
+    "SPECIES_HARIYAMA",
+    "SPECIES_AZURILL",
+    "SPECIES_NOSEPASS",
+    "SPECIES_SKITTY",
+    "SPECIES_DELCATTY",
+    "SPECIES_SABLEYE",
+    "SPECIES_MAWILE",
+    "SPECIES_ARON",
+    "SPECIES_LAIRON",
+    "SPECIES_AGGRON",
+    "SPECIES_MEDITITE",
+    "SPECIES_MEDICHAM",
+    "SPECIES_ELECTRIKE",
+    "SPECIES_MANECTRIC",
+    "SPECIES_PLUSLE",
+    "SPECIES_MINUN",
+    "SPECIES_VOLBEAT",
+    "SPECIES_ILLUMISE",
+    "SPECIES_ROSELIA",
+    "SPECIES_GULPIN",
+    "SPECIES_SWALOT",
+    "SPECIES_CARVANHA",
+    "SPECIES_SHARPEDO",
+    "SPECIES_WAILMER",
+    "SPECIES_WAILORD",
+    "SPECIES_NUMEL",
+    "SPECIES_CAMERUPT",
+    "SPECIES_TORKOAL",
+    "SPECIES_SPOINK",
+    "SPECIES_GRUMPIG",
+    "SPECIES_SPINDA",
+    "SPECIES_TRAPINCH",
+    "SPECIES_VIBRAVA",
+    "SPECIES_FLYGON",
+    "SPECIES_CACNEA",
+    "SPECIES_CACTURNE",
+    "SPECIES_SWABLU",
+    "SPECIES_ALTARIA",
+    "SPECIES_ZANGOOSE",
+    "SPECIES_SEVIPER",
+    "SPECIES_LUNATONE",
+    "SPECIES_SOLROCK",
+    "SPECIES_BARBOACH",
+    "SPECIES_WHISCASH",
+    "SPECIES_CORPHISH",
+    "SPECIES_CRAWDAUNT",
+    "SPECIES_BALTOY",
+    "SPECIES_CLAYDOL",
+    "SPECIES_LILEEP",
+    "SPECIES_CRADILY",
+    "SPECIES_ANORITH",
+    "SPECIES_ARMALDO",
+    "SPECIES_FEEBAS",
+    "SPECIES_MILOTIC",
+    "SPECIES_CASTFORM",
+    "SPECIES_KECLEON",
+    "SPECIES_SHUPPET",
+    "SPECIES_BANETTE",
+    "SPECIES_DUSKULL",
+    "SPECIES_DUSCLOPS",
+    "SPECIES_TROPIUS",
+    "SPECIES_CHIMECHO",
+    "SPECIES_ABSOL",
+    "SPECIES_WYNAUT",
+    "SPECIES_SNORUNT",
+    "SPECIES_GLALIE",
+    "SPECIES_SPHEAL",
+    "SPECIES_SEALEO",
+    "SPECIES_WALREIN",
+    "SPECIES_CLAMPERL",
+    "SPECIES_HUNTAIL",
+    "SPECIES_GOREBYSS",
+    "SPECIES_RELICANTH",
+    "SPECIES_LUVDISC",
+    "SPECIES_BAGON",
+    "SPECIES_SHELGON",
+    "SPECIES_SALAMENCE",
+    "SPECIES_BELDUM",
+    "SPECIES_METANG",
+    "SPECIES_METAGROSS",
+    "SPECIES_REGIROCK",
+    "SPECIES_REGICE",
+    "SPECIES_REGISTEEL",
+    "SPECIES_LATIAS",
+    "SPECIES_LATIOS",
+    "SPECIES_KYOGRE",
+    "SPECIES_GROUDON",
+    "SPECIES_RAYQUAZA",
+    "SPECIES_JIRACHI",
+    "SPECIES_DEOXYS",
+    "SPECIES_TURTWIG",
+    "SPECIES_GROTLE",
+    "SPECIES_TORTERRA",
+    "SPECIES_CHIMCHAR",
+    "SPECIES_MONFERNO",
+    "SPECIES_INFERNAPE",
+    "SPECIES_PIPLUP",
+    "SPECIES_PRINPLUP",
+    "SPECIES_EMPOLEON",
+    "SPECIES_STARLY",
+    "SPECIES_STARAVIA",
+    "SPECIES_STARAPTOR",
+    "SPECIES_BIDOOF",
+    "SPECIES_BIBAREL",
+    "SPECIES_KRICKETOT",
+    "SPECIES_KRICKETUNE",
+    "SPECIES_SHINX",
+    "SPECIES_LUXIO",
+    "SPECIES_LUXRAY",
+    "SPECIES_BUDEW",
+    "SPECIES_ROSERADE",
+    "SPECIES_CRANIDOS",
+    "SPECIES_RAMPARDOS",
+    "SPECIES_SHIELDON",
+    "SPECIES_BASTIODON",
+    "SPECIES_BURMY",
+    "SPECIES_WORMADAM",
+    "SPECIES_MOTHIM",
+    "SPECIES_COMBEE",
+    "SPECIES_VESPIQUEN",
+    "SPECIES_PACHIRISU",
+    "SPECIES_BUIZEL",
+    "SPECIES_FLOATZEL",
+    "SPECIES_CHERUBI",
+    "SPECIES_CHERRIM",
+    "SPECIES_SHELLOS",
+    "SPECIES_GASTRODON",
+    "SPECIES_AMBIPOM",
+    "SPECIES_DRIFLOON",
+    "SPECIES_DRIFBLIM",
+    "SPECIES_BUNEARY",
+    "SPECIES_LOPUNNY",
+    "SPECIES_MISMAGIUS",
+    "SPECIES_HONCHKROW",
+    "SPECIES_GLAMEOW",
+    "SPECIES_PURUGLY",
+    "SPECIES_CHINGLING",
+    "SPECIES_STUNKY",
+    "SPECIES_SKUNTANK",
+    "SPECIES_BRONZOR",
+    "SPECIES_BRONZONG",
+    "SPECIES_BONSLY",
+    "SPECIES_MIME_JR",
+    "SPECIES_HAPPINY",
+    "SPECIES_CHATOT",
+    "SPECIES_SPIRITOMB",
+    "SPECIES_GIBLE",
+    "SPECIES_GABITE",
+    "SPECIES_GARCHOMP",
+    "SPECIES_MUNCHLAX",
+    "SPECIES_RIOLU",
+    "SPECIES_LUCARIO",
+    "SPECIES_HIPPOPOTAS",
+    "SPECIES_HIPPOWDON",
+    "SPECIES_SKORUPI",
+    "SPECIES_DRAPION",
+    "SPECIES_CROAGUNK",
+    "SPECIES_TOXICROAK",
+    "SPECIES_CARNIVINE",
+    "SPECIES_FINNEON",
+    "SPECIES_LUMINEON",
+    "SPECIES_MANTYKE",
+    "SPECIES_SNOVER",
+    "SPECIES_ABOMASNOW",
+    "SPECIES_WEAVILE",
+    "SPECIES_MAGNEZONE",
+    "SPECIES_LICKILICKY",
+    "SPECIES_RHYPERIOR",
+    "SPECIES_TANGROWTH",
+    "SPECIES_ELECTIVIRE",
+    "SPECIES_MAGMORTAR",
+    "SPECIES_TOGEKISS",
+    "SPECIES_YANMEGA",
+    "SPECIES_LEAFEON",
+    "SPECIES_GLACEON",
+    "SPECIES_GLISCOR",
+    "SPECIES_MAMOSWINE",
+    "SPECIES_PORYGON_Z",
+    "SPECIES_GALLADE",
+    "SPECIES_PROBOPASS",
+    "SPECIES_DUSKNOIR",
+    "SPECIES_FROSLASS",
+    "SPECIES_ROTOM",
+    "SPECIES_UXIE",
+    "SPECIES_MESPRIT",
+    "SPECIES_AZELF",
+    "SPECIES_DIALGA",
+    "SPECIES_PALKIA",
+    "SPECIES_HEATRAN",
+    "SPECIES_REGIGIGAS",
+    "SPECIES_GIRATINA",
+    "SPECIES_CRESSELIA",
+    "SPECIES_PHIONE",
+    "SPECIES_MANAPHY",
+    "SPECIES_DARKRAI",
+    "SPECIES_SHAYMIN",
+    "SPECIES_ARCEUS"
+]
+
+import json
+import random
+import os
+import traceback # Import traceback for detailed error reporting
+
+
+# --- Configuration ---
+# Relative path to the JSON file from the script's directory
+relative_path = "src/data/wild_encounters.json"
+# Get the absolute path to the JSON file
+script_dir = os.path.dirname(__file__)  # Get the directory where the script is located
+json_file_path = os.path.join(script_dir, relative_path)
+
+# --- Main Logic ---
+try:
+    # Load the JSON data from the file
+    with open(json_file_path, 'r') as f:
+        data = json.load(f)
+    print(f"Successfully loaded '{json_file_path}'")
+
+    modified_count = 0
+    # Define the keys for different encounter types that contain 'mons' lists
+    encounter_types = ["land_mons", "water_mons", "rock_smash_mons", "fishing_mons"]
+
+    # Navigate through the structure to find the species entries
+    # Assumes the structure is consistent with the example provided
+    if "wild_encounter_groups" in data and len(data["wild_encounter_groups"]) > 0:
+        encounter_group = data["wild_encounter_groups"][0] # Assuming modifications are needed in the first group
+        if "encounters" in encounter_group:
+            for encounter in encounter_group["encounters"]:
+                for encounter_type in encounter_types:
+                    # Check if the encounter type exists for this map and has a 'mons' list
+                    if encounter_type in encounter and "mons" in encounter[encounter_type] and isinstance(encounter[encounter_type]["mons"], list):
+                        for mon in encounter[encounter_type]["mons"]:
+                            # Check if the 'species' key exists
+                            if "species" in mon:
+                                # Replace the species with a random one from the list
+                                original_species = mon["species"]
+                                mon["species"] = random.choice(pokemon_species_list)
+                                # Optional: Print which species was changed
+                                # print(f"  Replaced {original_species} with {mon['species']}")
+                                modified_count += 1
+
+    # Save the modified data back to the original file
+    with open(json_file_path, 'w') as f:
+        # Use indent for pretty printing, making the file readable
+        json.dump(data, f, indent=2) # Use indent=4 if you prefer 4 spaces
+
+    print(f"Successfully modified and saved '{json_file_path}'.")
+    print(f"Total species entries randomized: {modified_count}")
+
+except FileNotFoundError:
+    print(f"Error: The file '{json_file_path}' was not found.")
+    print("Please ensure the script is in the correct directory relative to 'src/data/'.")
+except json.JSONDecodeError:
+    print(f"Error: Could not decode JSON from '{json_file_path}'. Check if the file is valid JSON.")
+except KeyError as e:
+    print(f"Error: Missing expected key {e} in the JSON structure. Check the JSON format.")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
+
+
+
+
+
+
+import random
+import re
+import os
+
+def update_starters_in_file(pokemon_list, file_path):
+    num_starters_to_choose = 3
+
+    if not os.path.exists(file_path):
+        print(f"Error: File not found at '{file_path}'")
+        return
+
+    if len(pokemon_list) < num_starters_to_choose:
+        print(f"Error: Need at least {num_starters_to_choose} Pokémon in list, have {len(pokemon_list)}")
+        return
+
+    try:
+        chosen_pokemon = random.sample(pokemon_list, num_starters_to_choose)
+    except ValueError:
+        print(f"Error sampling {num_starters_to_choose} unique items from a list of size {len(pokemon_list)}.")
+        return
+
+    formatted_starters = [f"SPECIES_{name.upper()}" for name in chosen_pokemon]
+    new_array_content = ",\n    ".join(formatted_starters)
+    new_array_content = f"    {new_array_content},\n"
+
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            original_content = f.read()
+    except Exception as e:
+        print(f"Error reading file '{file_path}': {e}")
+        return
+
+    pattern = re.compile(
+        r'(static\s+const\s+u16\s+sStarterMon\[STARTER_MON_COUNT\]\s*=\s*)'
+        r'{\s*.*?\s*};',
+        re.DOTALL
+    )
+
+    replacement_string = r'\1' + f"{{\n{new_array_content}}};"
+
+    modified_content, num_replacements = pattern.subn(replacement_string, original_content, count=1)
+
+    if num_replacements == 0:
+        print(f"Error: Could not find the sStarterMon array definition in '{file_path}'. Check the file content and structure.")
+        return
+
+    try:
+        with open(file_path, 'w', encoding='utf-8') as f:
+            f.write(modified_content)
+        print(f"Successfully updated starters in '{file_path}' to: {', '.join(chosen_pokemon)}")
+    except Exception as e:
+        print(f"Error writing updated content to file '{file_path}': {e}")
+
+
+c_file_target = "src/starter_choose.c"
+
+update_starters_in_file(pokemon_species_list, c_file_target)
